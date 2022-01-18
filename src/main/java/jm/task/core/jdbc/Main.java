4b5851005b29178dc34f7fccd589905118231f3c
Hoochee <jok777@yandex.ru>
2022-01-18 13:30:11 +0300

    Practical task 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,32 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        final UserService userService = new UserServiceImpl();
+        List<User> users = new ArrayList<>();
+        users.add(new User("Ivan", "Ivanov", (byte) 25));
+        users.add(new User("Petr", "Petrov", (byte) 26));
+        users.add(new User("Maks", "Ivanov", (byte) 27));
+        users.add(new User("Sava", "Ivanov", (byte) 28));
+
+        userService.createUsersTable();
+        for (User user : users) {
+            userService.saveUser(user.getName(), user.getLastName(), user.getAge());
+            System.out.println("User с именем – " + user.getName() + " добавлен в базу данных");
+        }
+        for (User user : userService.getAllUsers()) {
+            System.out.println(user);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+        Util.closeConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,85 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
-
     }
 
     public void createUsersTable() {
-
+        final String createTable = "CREATE TABLE IF NOT EXISTS Users("
+                + "id BIGINT PRIMARY KEY AUTO_INCREMENT,"
+                + "name VARCHAR(15),"
+                + "lastName VARCHAR(15),"
+                + "age TINYINT)";
+        final String autoIncrement = "ALTER TABLE users AUTO_INCREMENT=10001";
+        try {
+            Util.getStatement().executeUpdate(createTable);
+            Util.getStatement().executeUpdate(autoIncrement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        final String dropTable = "DROP TABLE IF EXISTS Users";
+        try {
+            Util.getStatement().executeUpdate(dropTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        final String sql = "INSERT INTO Users VALUES(NULL,'" + name + "','" + lastName + "','" + age + "')";
+        try {
+            Util.getStatement().executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void removeUserById(long id) {
-
+        final String removeUser = "DELETE FROM Users WHERE id = " + id;
+        try {
+            Util.getStatement().executeUpdate(removeUser);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM Users";
+        try {
+            ResultSet resultSet = Util.getStatement().executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        final String cleanTable = "DELETE FROM Users";
+        try {
+            Util.getStatement().executeUpdate(cleanTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
